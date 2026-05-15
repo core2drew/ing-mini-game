ed63bd9bf19c30227a363faba8ffacc9bf7d6210
core2drew <calupesandro@gmail.com>
2026-05-15 14:04:00 +0800

    chore: add login page route

---
+++ src/app/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LucideZap } from '@lucide/angular';
+
+@Component({
+  selector: 'app-login',
+  imports: [LucideZap],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {}
