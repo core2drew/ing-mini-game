5ba41bf149420e9cb9f4df24eb1bdf168d9135cb
core2drew <calupesandro@gmail.com>
2026-05-15 20:37:19 +0800

    chore: use input mask

--- src/app/join-room/join-room.ts
+++ src/app/join-room/join-room.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideZap } from '@lucide/angular';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-join-room',
-  imports: [LucideZap],
+  imports: [LucideZap, InputMaskModule, InputTextModule, FormsModule],
   templateUrl: './join-room.html',
   styleUrl: './join-room.css',
+  standalone: true,
 })
-export class JoinRoom {}
+export class JoinRoom {
+  value: string | undefined;
+}
