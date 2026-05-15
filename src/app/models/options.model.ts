ecacd00f3d72c4aa5418dca9bda48e32e00f470e
core2drew <calupesandro@gmail.com>
2026-05-16 00:23:03 +0800

    chore: create option colors model

---
+++ src/app/models/options.model.ts
@@ -0,0 +1,6 @@
+export const OPTION_COLORS = [
+  { bg: 'bg-blue-500 hover:bg-blue-400', selected: 'bg-blue-600', label: 'A' },
+  { bg: 'bg-green-500 hover:bg-green-400', selected: 'bg-green-600', label: 'B' },
+  { bg: 'bg-amber-500 hover:bg-amber-400', selected: 'bg-amber-600', label: 'C' },
+  { bg: 'bg-red-500 hover:bg-red-400', selected: 'bg-red-600', label: 'D' },
+];
