547d8cf9c59dbdfb00f60b22ae9095d56d1f730b
core2drew <calupesandro@gmail.com>
2026-05-16 15:45:45 +0800

    chore: add loading state to button on click

--- src/app/pages/join-room-page/components/room-step/room-step.ts
+++ src/app/pages/join-room-page/components/room-step/room-step.ts
@@ -1,30 +1,53 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RoomService } from '@services/room/room.service';
+import { MessageService } from 'primeng/api';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
-import { model } from '@angular/core';
+import { ToastModule } from 'primeng/toast';
+import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-room-step',
-  imports: [InputMaskModule, InputTextModule, FloatLabelModule, FormsModule],
+  imports: [
+    InputMaskModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule,
+    ToastModule,
+    RippleModule,
+    ButtonModule,
+  ],
   templateUrl: './room-step.html',
   styleUrl: './room-step.css',
   standalone: true,
+  providers: [MessageService],
 })
 export class RoomStep {
   @Output() nextStep = new EventEmitter<number>();
+  loading = false;
   roomId = ''; // Initialize roomId as a model of type string
-
+  private messageService = inject(MessageService);
   constructor(private roomService: RoomService) {}
 
   joinRoom() {
-    this.roomService.joinRoom(this.roomId!).then((exists) => {
-      if (exists) {
-        this.nextStep.emit(2);
-      } else {
-        alert('Room does not exist');
-      }
-    });
+    this.loading = true;
+    this.roomService
+      .joinRoom(this.roomId!)
+      .then((exists) => {
+        if (exists) {
+          this.nextStep.emit(2);
+        } else {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Room does not exist',
+          });
+        }
+      })
+      .finally(() => {
+        this.loading = false;
+      });
   }
 }
