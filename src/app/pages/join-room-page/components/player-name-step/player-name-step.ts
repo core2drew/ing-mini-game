8efcac055401fbbef2f5d828469c6bd32733eb6f
core2drew <calupesandro@gmail.com>
2026-05-16 19:54:27 +0800

    refactor: integrate player service to create player

--- src/app/pages/join-room-page/components/player-name-step/player-name-step.ts
+++ src/app/pages/join-room-page/components/player-name-step/player-name-step.ts
@@ -1,8 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
+import { playerStore } from '@stores/player.store';
+import { PlayerService } from '@services/quiz/player.service';
+import { MessageService } from 'primeng/api';
 import { MessageModule } from 'primeng/message';
 @Component({
   selector: 'app-player-name-step',
@@ -14,11 +17,28 @@ import { MessageModule } from 'primeng/message';
 export class PlayerNameStep {
   @Output() nextStep = new EventEmitter<number>();
   loading = false;
-  playerName: any;
+  playerName: string | null = null;
+
+  private playerService = inject(PlayerService);
+  private messageService = inject(MessageService);
 
   enterGame(form: any) {
     if (form.valid) {
-      this.nextStep.emit(3);
+      this.playerService.createPlayer(this.playerName!).subscribe({
+        next: (player) => {
+          playerStore.update((state) => ({ ...state, name: player.name }));
+          this.nextStep.emit(3);
+        },
+        error: (error) => {
+          this.messageService.add({
+            severity: 'error',
+            styleClass: 'border-none',
+            contentStyleClass: 'bg-slate-800/60',
+            summary: 'Error',
+            detail: error.message,
+          });
+        },
+      });
     }
   }
 }
