b39d6344d2de8bc2149b6457ffae1f4b28e0df04
core2drew <calupesandro@gmail.com>
2026-05-16 11:50:15 +0800

    chore: refactor question service name

--- src/app/quiz-page/quiz-page.ts
+++ src/app/quiz-page/quiz-page.ts
@@ -3,9 +3,9 @@ import { QuestionText } from './components/question-text/question-text';
 import { QuestionOptionButton } from './components/question-option-button/question-option-button';
 import { QuizProgress } from './components/quiz-progress/quiz-progress';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { Question } from '@models/quiz/question.model';
-import { QuestionsService } from '@services/quiz/question.service';
+import { QuestionService } from '@services/quiz/question.service';
 
 @Component({
   selector: 'app-quiz-page',
@@ -15,15 +15,22 @@ import { QuestionsService } from '@services/quiz/question.service';
   standalone: true,
 })
 export class QuizPage {
-  currentQuestion$ = new BehaviorSubject(0);
+  playerId: string = '';
   questions: Question[] = [];
+
+  currentQuestion$ = new BehaviorSubject(0);
   revealed$ = new BehaviorSubject(false);
   selected$ = new BehaviorSubject<number | null>(null);
+  answers$ = new BehaviorSubject<number[]>([]);
+  score$ = new BehaviorSubject(0);
+  finished$ = new BehaviorSubject(false);
 
-  constructor(private questionsService: QuestionsService) {}
+  private destroy$ = new Subject<void>();
+
+  constructor(private questionService: QuestionService) {}
 
   ngOnInit(): void {
-    this.questions = this.questionsService.getQuestions();
+    this.questions = this.questionService.getQuestions();
   }
 
   get progressPercent(): number {
