20ae98844f9c8b9c9246b3e7a4193bcfc5a8d57d
core2drew <calupesandro@gmail.com>
2026-05-16 10:55:37 +0800

    chore: include current question number and question length

--- src/app/quiz-page/components/quiz-progress/quiz-progress.ts
+++ src/app/quiz-page/components/quiz-progress/quiz-progress.ts
@@ -1,12 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-progress',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz-progress.html',
   styleUrl: './quiz-progress.css',
   standalone: true,
 })
 export class QuizProgress {
   @Input() progress: number = 0;
+  @Input() currentQuestionNumber: number = 1;
+  @Input() questionLength: number = 1;
 }
