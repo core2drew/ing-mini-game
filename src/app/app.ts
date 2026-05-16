074bf92aa8a739f388d2debb8f5010275717c390
core2drew <calupesandro@gmail.com>
2026-05-16 19:52:27 +0800

    feat: integrate toast to whole app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [MessageService],
 })
-export class App {
-  protected readonly title = signal('ing-mini-game');
-}
+export class App {}
