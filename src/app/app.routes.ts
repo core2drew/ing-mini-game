06167ad3a234dd721e2c29bdc40a5bb54961f953
core2drew <calupesandro@gmail.com>
2026-05-16 12:38:18 +0800

    chore: update route paths - redirect unknown to join

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
-import { JoinRoom } from './join-room/join-room';
-import { QuizPage } from './quiz-page/quiz-page';
+import { JoinRoomPage } from './pages/join-room-page/join-room-page';
+import { QuizPage } from './pages/quiz-page/quiz-page';
+import { CreateRoomPage } from './pages/create-room-page/create-room-page';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: JoinRoom,
+    path: 'join',
+    component: JoinRoomPage,
+  },
+  {
+    path: 'create-room',
+    component: CreateRoomPage,
   },
   {
     path: 'quiz-blitz',
     component: QuizPage,
+  },
+  {
+    path: '**',
+    redirectTo: 'join',
+    pathMatch: 'full',
   },
 ];
