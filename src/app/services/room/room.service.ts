9b8cccb2d4ece76fb0c763e1fe1c8582cd9dbe97
core2drew <calupesandro@gmail.com>
2026-05-16 15:44:58 +0800

    chore: rename collection name

--- src/app/services/room/room.service.ts
+++ src/app/services/room/room.service.ts
@@ -9,7 +9,7 @@ export class RoomService {
   constructor(private firebaseService: FirebaseService) {}
 
   async joinRoom(roomCode: string): Promise<boolean> {
-    const roomRef = doc(this.firebaseService.getDb(), 'Rooms', roomCode);
+    const roomRef = doc(this.firebaseService.getDb(), 'rooms', roomCode);
     const roomSnap = await getDoc(roomRef);
     return roomSnap.exists();
   }
