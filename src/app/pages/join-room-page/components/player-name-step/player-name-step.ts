f6ef91374ac756e561e3769114f43e3c5f3030d6
core2drew <calupesandro@gmail.com>
2026-05-16 16:04:16 +0800

    chore: add validation for player name

--- src/app/pages/join-room-page/components/player-name-step/player-name-step.ts
+++ src/app/pages/join-room-page/components/player-name-step/player-name-step.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
-
+import { MessageModule } from 'primeng/message';
 @Component({
   selector: 'app-player-name-step',
-  imports: [FloatLabelModule, InputTextModule],
+  imports: [FloatLabelModule, InputTextModule, FormsModule, ButtonModule, MessageModule],
   templateUrl: './player-name-step.html',
   styleUrl: './player-name-step.css',
   standalone: true,
 })
 export class PlayerNameStep {
   @Output() nextStep = new EventEmitter<number>();
+  loading = false;
+  playerName: any;
 
-  joinGame() {
-    this.nextStep.emit(3);
+  enterGame(form: any) {
+    if (form.valid) {
+      this.nextStep.emit(3);
+    }
   }
 }
