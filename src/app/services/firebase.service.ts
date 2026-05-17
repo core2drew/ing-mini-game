af6323d1c78dfcc66b4c61b231d87c893c413802
core2drew <calupesandro@gmail.com>
2026-05-17 22:26:20 +0800

    feat: integrate @angular/fire auth

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
+import { getAuth, User } from 'firebase/auth';
 import { getFirestore, Firestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { environment } from '../../environments/environment';
+import { Auth, authState } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
   private db!: Firestore;
-
+  private auth!: Auth;
+  private authState!: Observable<User | null>;
+  private currentUser: User | null = null;
   constructor() {
     this.initializeFirebase();
   }
@@ -16,7 +21,9 @@ export class FirebaseService {
   private initializeFirebase() {
     const app = initializeApp(environment.firebase);
     this.db = getFirestore(app);
-
+    this.auth = getAuth(app);
+    this.authState = authState(this.auth);
+    this.currentUser = this.auth.currentUser;
     enableIndexedDbPersistence(this.db).catch((err) => {
       if (err.code === 'failed-precondition') {
         console.warn('Multiple tabs open, persistence only enabled in one tab at a time.');
@@ -30,5 +37,17 @@ export class FirebaseService {
 
   getDb(): Firestore {
     return this.db;
+  }
+
+  getAuthState() {
+    return this.authState;
+  }
+
+  getAuth(): Auth {
+    return this.auth;
+  }
+
+  getCurrentUser() {
+    return this.currentUser;
   }
 }
