5038bccb7a130a4c57937ef1e66c653d49ebd1eb
core2drew <calupesandro@gmail.com>
2026-05-17 22:27:12 +0800

    chore: integrate auth guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { roomGuard } from './guards/room-guard/room-guard';
+import { authGuard } from './guards/auth/auth-guard';
+import { unauthGuard } from './guards/unauth/unauth-guard';
 
 export const routes: Routes = [
   {
@@ -12,14 +14,23 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/quiz-page/quiz-page').then((m) => m.QuizPage),
     canActivate: [roomGuard],
   },
+
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login),
+    canActivate: [unauthGuard],
+  },
   {
     path: 'create-room',
     loadComponent: () =>
       import('./pages/create-room-page/create-room-page').then((m) => m.CreateRoomPage),
+    canActivate: [authGuard],
   },
   {
-    path: 'login',
-    loadComponent: () => import('./pages/auth/login/login').then((m) => m.Login),
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard-page/dashboard-page').then((m) => m.DashboardPage),
+    canActivate: [authGuard],
   },
   {
     path: '**',
