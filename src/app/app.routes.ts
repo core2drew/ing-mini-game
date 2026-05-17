b30c99d44373210465e71ffa553379ad616fa3fa
core2drew <calupesandro@gmail.com>
2026-05-17 15:30:03 +0800

    chore: integrate room guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { JoinRoomPage } from './pages/join-room-page/join-room-page';
 import { QuizPage } from './pages/quiz-page/quiz-page';
 import { CreateRoomPage } from './pages/create-room-page/create-room-page';
+import { roomGuard } from './guards/room-guard/room-guard';
 
 export const routes: Routes = [
   {
@@ -9,12 +10,13 @@ export const routes: Routes = [
     component: JoinRoomPage,
   },
   {
-    path: 'create-room',
-    component: CreateRoomPage,
-  },
-  {
     path: 'quiz-blitz',
     component: QuizPage,
+    canActivate: [roomGuard],
+  },
+  {
+    path: 'create-room',
+    component: CreateRoomPage,
   },
   {
     path: '**',
