ed9a48f1334580cdef6886487c0034b7f40d95cb
core2drew <calupesandro@gmail.com>
2026-05-18 19:23:28 +0800

    chore: add providers for firebase, firestory and firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,13 @@ import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideFirestore } from '@angular/fire/firestore';
+import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { environment } from '@environments/environment';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +20,8 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
 };
