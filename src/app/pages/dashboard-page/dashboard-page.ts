e45aba802f9c92c1b0a8155558314494880285a7
core2drew <calupesandro@gmail.com>
2026-05-20 13:44:47 +0800

    chore: add action in dashboard

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '@services/auth/auth.service';
 import { ButtonModule } from 'primeng/button';
-
+import { AdminService } from '@services/admin/admin.service';
 @Component({
   selector: 'app-dashboard-page',
   imports: [ButtonModule],
@@ -10,8 +10,24 @@ import { ButtonModule } from 'primeng/button';
 })
 export class DashboardPage {
   private authService = inject(AuthService);
+  private adminService = inject(AdminService);
 
   logout() {
     this.authService.signOut();
+  }
+
+  startQuiz() {
+    const roomId = '123-123-123'; // This would be dynamic in a real app
+    this.adminService.startQuizSession(roomId);
+  }
+
+  restartQuiz() {
+    const roomId = '123-123-123'; // This would be dynamic in a real app
+    this.adminService.restartQuizSession(roomId);
+  }
+
+  endQuiz() {
+    const roomId = '123-123-123'; // This would be dynamic in a real app
+    this.adminService.endQuizSession(roomId);
   }
 }
