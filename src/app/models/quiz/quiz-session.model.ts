e4922047822561ddee5aab9d0954b6e45b5a4c08
core2drew <calupesandro@gmail.com>
2026-05-22 22:23:39 +0800

    refactor: remove currentQuestionId from QuizSession interface

--- src/app/models/quiz/quiz-session.model.ts
+++ src/app/models/quiz/quiz-session.model.ts
@@ -1,7 +1,6 @@
 import { Timestamp } from 'firebase/firestore';
 
 export interface QuizSession {
-  currentQuestionId: string;
   currentQuestionIndex: number;
   questionTimerExpiresAt: Timestamp;
 }
