522a928ad3dccdcf564467debf8497bd480fbad3
core2drew <calupesandro@gmail.com>
2026-05-24 19:21:23 +0800

    chore: add question screen to check wrong / correct screen

--- src/app/pages/quiz-page/components/screens/question-screen/question-screen.ts
+++ src/app/pages/quiz-page/components/screens/question-screen/question-screen.ts
@@ -1,11 +1,11 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, computed, inject, model, signal, Signal } from '@angular/core';
 import { QuizTimer } from '../../quiz-timer/quiz-timer';
 import { QuizProgress } from '../../quiz-progress/quiz-progress';
 import { QuestionText } from '../../question-text/question-text';
 import { QuestionOptionButton } from '../../question-option-button/question-option-button';
 import { playerStore } from '@stores/player.store';
 import { GameService } from '@services/quiz/game.service';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Question } from '@models/quiz/question.model';
 import { QuestionService } from '@services/quiz/question.service';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -16,6 +16,7 @@ import { CommonModule } from '@angular/common';
   imports: [QuizTimer, QuizProgress, QuestionText, QuestionOptionButton, CommonModule],
   templateUrl: './question-screen.html',
   styleUrl: './question-screen.css',
+  standalone: true,
 })
 export class QuestionScreen {
   private gameService = inject(GameService);
@@ -23,11 +24,20 @@ export class QuestionScreen {
   timer$!: Observable<number>;
   revealed$ = new BehaviorSubject(false);
   currentQuestionIndex$ = new BehaviorSubject(0);
-  selected$ = new BehaviorSubject<number | null>(null);
+  readonly selected = signal<number | null>(null);
 
   activeQuestion = toSignal(
     this.questionService.watchActiveQuestion(playerStore.getValue().roomId!),
   );
+
+  wrongScreenActive = model.required<boolean>();
+  correctScreenActive = model.required<boolean>();
+
+  readonly answerIsCorrect = computed(() => {
+    const selectedIndex = this.selected();
+    if (selectedIndex === null) return false;
+    return selectedIndex === this.activeQuestion()?.correctIndex;
+  });
 
   get isRevealed(): boolean {
     return this.revealed$.value;
@@ -42,21 +52,28 @@ export class QuestionScreen {
   }
 
   ngOnInit() {
-    // 1. Fetch the active roomId out of your Elf store
     const currentRoomId = playerStore.getValue().roomId;
-
-    // Convert the real-time Firebase observable straight into a read-only Signal!
     this.timer$ = this.gameService.streamGameRoomTimer(currentRoomId!);
   }
 
-  handleTimeRanOut() {
+  timeRanOut() {
     // Lock inputs, auto-submit selected answer if any, wait for host next step
     console.log('Time is up for this question!');
+    this.revealed$.next(true);
+    setTimeout(() => {
+      this.wrongScreenActive.set(true);
+    }, 900);
   }
 
   onSelectOption(idx: number): void {
-    if (this.selected$.value !== null) return;
-    this.selected$.next(idx);
-    this.revealed$.next(true);
+    if (this.selected() !== null) return;
+    this.selected.set(idx);
+    setTimeout(() => {
+      if (this.selected() === this.activeQuestion()?.correctIndex) {
+        this.correctScreenActive.set(true);
+      } else {
+        this.wrongScreenActive.set(true);
+      }
+    }, 900);
   }
 }
