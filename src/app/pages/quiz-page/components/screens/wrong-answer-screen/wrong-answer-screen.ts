be7e8752c709b9e871db9d0db1762ed7aa74cbf6
core2drew <calupesandro@gmail.com>
2026-05-25 21:21:59 +0800

    feat: add inspirational message randomizer

--- src/app/pages/quiz-page/components/screens/wrong-answer-screen/wrong-answer-screen.ts
+++ src/app/pages/quiz-page/components/screens/wrong-answer-screen/wrong-answer-screen.ts
@@ -1,10 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { LucideX } from '@lucide/angular';
+import { LucideX, LucideCheck } from '@lucide/angular';
 
 @Component({
   selector: 'app-wrong-answer-screen',
-  imports: [LucideX],
+  imports: [LucideX, LucideCheck],
   templateUrl: './wrong-answer-screen.html',
   styleUrl: './wrong-answer-screen.css',
   standalone: true,
@@ -18,6 +18,21 @@ export class WrongAnswerScreen {
   }));
   showContent = signal(true);
   shakeActive = signal(true);
+
+  getRandomInspirationalMessage() {
+    const inspirationMessages = [
+      `Just call it a "creative choice" and move on.`,
+      `On the bright side, you just learned one definitive way not to do it.`,
+      `Erasers exist for a reason. If we were perfect, pencils wouldn't have them.`,
+      `Don't worry, nobody was looking (hopefully).`,
+      `Well, at least you gave the universe a plot twist.`,
+      `Perfect people are boring anyway.`,
+      `Don't worry — every mistake is just a lesson in disguise.`,
+      `At least you're being consistent at keeping life unpredictable.`,
+      `That wasn't a blunder, it was a highly calculated risk... that just happened to backfire.`,
+    ];
+    return inspirationMessages[Math.floor(Math.random() * inspirationMessages.length)];
+  }
 
   exitQuiz(): void {
     this.router.navigate(['/']);
