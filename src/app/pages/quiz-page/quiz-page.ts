ac7de8f415c08cc9855aead338debabc3c1e69ed
core2drew <calupesandro@gmail.com>
2026-05-25 18:29:00 +0800

    feat: refactor quiz page to use signals for active question and timer

--- src/app/pages/quiz-page/quiz-page.ts
+++ src/app/pages/quiz-page/quiz-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, Signal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { playerStore } from '@stores/player.store';
@@ -7,6 +7,10 @@ import { RoomService } from '@services/room/room.service';
 import { QuestionScreen } from './components/screens/question-screen/question-screen';
 import { WrongAnswerScreen } from './components/screens/wrong-answer-screen/wrong-answer-screen';
 import { CorrectAnswerScreen } from './components/screens/correct-answer-screen/correct-answer-screen';
+import { QuestionService } from '@services/quiz/question.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { GameService } from '@services/quiz/game.service';
+import { Question } from '@models/quiz/question.model';
 @Component({
   selector: 'app-quiz-page',
   imports: [CommonModule, QuestionScreen, WrongAnswerScreen, CorrectAnswerScreen],
@@ -17,38 +21,37 @@ import { CorrectAnswerScreen } from './components/screens/correct-answer-screen/
 export class QuizPage {
   playerId: string = '';
   secondsLeft = 0;
-
-  currentQuestion$ = new BehaviorSubject(0);
-  revealed$ = new BehaviorSubject(false);
-  selected$ = new BehaviorSubject<number | null>(null);
-  answers$ = new BehaviorSubject<number[]>([]);
-
+  questionTimer: Signal<number | undefined> = signal(0);
+  activeQuestion: Signal<Question | undefined> = signal(undefined);
   finished$ = new BehaviorSubject(false);
 
   private gameEndSub!: Subscription;
 
   private router = inject(Router);
   private roomService = inject(RoomService);
+  private questionService = inject(QuestionService);
+  private gameService = inject(GameService);
+  private currentRoomId = playerStore.getValue().roomId;
 
-  wrongScreenActive = signal(false);
-  correctScreenActive = signal(false);
+  wrongScreenActive = this.questionService.wrongAnswer;
+  correctScreenActive = this.questionService.correctAnswer;
 
-  ngOnInit(): void {
-    // 1. Fetch the active roomId out of your Elf store
-    const currentRoomId = playerStore.getValue().roomId;
-
-    if (!currentRoomId) {
+  constructor() {
+    if (!this.currentRoomId) {
       console.error('No active room found, redirecting back to home.');
       this.router.navigate(['/']);
       return;
     }
+    this.activeQuestion = toSignal(
+      this.questionService.watchActiveQuestion(playerStore.getValue().roomId!),
+    );
 
-    // 2. Start watching for the host to click "End Game"
-    this.gameEndSub = this.roomService.waitUntilGameEnds(currentRoomId).subscribe({
+    this.questionTimer = toSignal(this.gameService.streamGameRoomTimer(this.currentRoomId!));
+
+    this.gameEndSub = this.roomService.waitUntilGameEnds(this.currentRoomId).subscribe({
       next: (isEnded) => {
         if (isEnded) {
           console.log('Game has ended! Redirecting to arena...');
-          // 3. Move the player out of the lobby into the active match
           this.router.navigate(['/join']);
         }
       },
