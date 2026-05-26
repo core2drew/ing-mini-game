b11a445cc59556b86101a5f53a69e4861a78d070
core2drew <calupesandro@gmail.com>
2026-05-26 14:49:54 +0800

    chore: update dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,11 @@ export const routes: Routes = [
   {
     path: 'join',
     loadComponent: () =>
-      import('./pages/join-room-page/join-room-page').then((m) => m.JoinRoomPage),
+      import(`./pages/join-room-page/join-room-page`).then((m) => m.JoinRoomPage),
   },
   {
     path: 'quiz-blitz',
-    loadComponent: () => import('./pages/quiz-page/quiz-page').then((m) => m.QuizPage),
+    loadComponent: () => import(`./pages/quiz-page/quiz-page`).then((m) => m.QuizPage),
     canActivate: [roomGuard],
   },
 
@@ -21,20 +21,40 @@ export const routes: Routes = [
     canActivate: [unauthGuard],
   },
   {
-    path: 'create-room',
-    loadComponent: () =>
-      import('./pages/create-room-page/create-room-page').then((m) => m.CreateRoomPage),
-    canActivate: [authGuard],
-  },
-  {
-    path: 'dashboard',
+    path: '',
     loadComponent: () =>
       import('./pages/dashboard-page/dashboard-page').then((m) => m.DashboardPage),
     canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/dashboard-page/pages/dashboard-page/dashboard-page').then(
+            (m) => m.DashboardPage,
+          ),
+      },
+      {
+        path: 'create-room',
+        loadComponent: () =>
+          import(`./pages/dashboard-page/pages/create-room-page/create-room-page`).then(
+            (m) => m.CreateRoomPage,
+          ),
+      },
+      {
+        path: 'room/:roomId',
+        loadComponent: () =>
+          import(`./pages/dashboard-page/pages/room-page/room-page`).then((m) => m.RoomPage),
+      },
+      {
+        path: '**',
+        redirectTo: '/',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '**',
-    redirectTo: 'join',
+    redirectTo: '/join',
     pathMatch: 'full',
   },
 ];
