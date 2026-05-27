2df940cb994b396f9695bd1939f6249faaaa9958
core2drew <calupesandro@gmail.com>
2026-05-27 16:32:54 +0800

    chore: add player status styles

--- src/app/pages/dashboard-page/pages/room-page/components/player-list/player-list.ts
+++ src/app/pages/dashboard-page/pages/room-page/components/player-list/player-list.ts
@@ -1,14 +1,30 @@
 import { Component, input } from '@angular/core';
 import { Avatar } from '../../../../../../components/avatar/avatar';
-import { Player } from '@models/quiz/player.model';
+import { PlayerStatus, PlayerWithUIData } from '@models/quiz/player.model';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
+import { ChipModule } from 'primeng/chip';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-player-list',
-  imports: [CardModule, DividerModule, Avatar],
+  imports: [CardModule, DividerModule, Avatar, ChipModule, CommonModule],
   templateUrl: './player-list.html',
   styleUrl: './player-list.css',
 })
 export class PlayerList {
-  playersAvatars = input<Player[] | undefined>(undefined);
+  playersAvatars = input<PlayerWithUIData[] | undefined>(undefined);
+
+  getStatusStyle(status: PlayerStatus | undefined) {
+    if (status) {
+      return {
+        waiting: status === PlayerStatus.WAITING,
+        thinking: status === PlayerStatus.THINKING,
+        answered: status === PlayerStatus.ANSWERED,
+        offline: status === PlayerStatus.OFFLINE,
+      };
+    }
+    return {
+      unknown: true,
+    };
+  }
 }
