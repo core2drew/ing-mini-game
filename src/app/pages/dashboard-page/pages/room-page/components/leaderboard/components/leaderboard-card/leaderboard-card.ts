ef37c8516ad4be6d9cdd455f24f2f8602cf4e8c4
core2drew <calupesandro@gmail.com>
2026-05-27 19:18:57 +0800

    chore: remove unused import

--- src/app/pages/dashboard-page/pages/room-page/components/leaderboard/components/leaderboard-card/leaderboard-card.ts
+++ src/app/pages/dashboard-page/pages/room-page/components/leaderboard/components/leaderboard-card/leaderboard-card.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Player, PlayerStatus } from '@models/quiz/player.model';
+import { Player } from '@models/quiz/player.model';
 import { Avatar } from '../../../../../../../../components/avatar/avatar';
 import { PlayerStatusChip } from '../../../player-status-chip/player-status-chip';
 
