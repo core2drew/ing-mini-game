087779dd3d751a1bd58169911c27093392555be6
core2drew <calupesandro@gmail.com>
2026-05-27 14:13:41 +0800

    chore: show players

--- src/app/pages/dashboard-page/pages/room-page/components/player-list/player-list.ts
+++ src/app/pages/dashboard-page/pages/room-page/components/player-list/player-list.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Avatar } from '../../../../../../components/avatar/avatar';
+import { Player } from '@models/quiz/player.model';
 import { CardModule } from 'primeng/card';
-
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-player-list',
-  imports: [CardModule],
+  imports: [CardModule, DividerModule, Avatar],
   templateUrl: './player-list.html',
   styleUrl: './player-list.css',
 })
-export class PlayerList {}
+export class PlayerList {
+  playersAvatars = input<Player[] | undefined>(undefined);
+}
