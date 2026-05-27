9f6046073c244f8a45720af3e72216464574b782
core2drew <calupesandro@gmail.com>
2026-05-28 07:49:15 +0800

    chore: improve rejoining in join step

--- src/app/pages/join-room-page/join-room-page.ts
+++ src/app/pages/join-room-page/join-room-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { Stepper } from './components/stepper/stepper';
 import { SessionService } from '@services/session/session.service';
 @Component({
@@ -11,11 +11,13 @@ import { SessionService } from '@services/session/session.service';
 export class JoinRoomPage {
   private sessionService = inject(SessionService);
 
-  currentStep: number = 1;
+  readonly currentStep = computed(() => {
+    const roomId = this.sessionService.roomId();
+    const playerName = this.sessionService.playerName();
 
-  ngOnInit() {
-    if (this.sessionService.isLoggedIn()) {
-      this.currentStep = 3;
-    }
-  }
+    if (roomId && playerName) return 3;
+    if (roomId) return 2;
+
+    return 1;
+  });
 }
