a8997ed159042487f8cb63bc00f5d4b707d80272
core2drew <calupesandro@gmail.com>
2026-05-28 02:23:29 +0800

    chore: clear session when wrong answer

--- src/app/pages/quiz-page/components/screens/question-screen/question-screen.ts
+++ src/app/pages/quiz-page/components/screens/question-screen/question-screen.ts
@@ -57,7 +57,7 @@ export class QuestionScreen {
     } else {
       this.wrongAnswer.set(true);
       this.gameService.setPlayerStatus(PlayerStatus.OFFLINE);
-      this.sessionService.resetSession();
+      this.sessionService.clearSession();
     }
   }
 
