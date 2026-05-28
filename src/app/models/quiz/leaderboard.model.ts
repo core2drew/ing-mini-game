7aa602b281fb8ea165119cd666cbe7c963e01d31
core2drew <calupesandro@gmail.com>
2026-05-28 11:20:13 +0800

    chore: add leadeboard model

---
+++ src/app/models/quiz/leaderboard.model.ts
@@ -0,0 +1,10 @@
+interface LeaderboardRow {
+  rank: number;
+  id: string;
+  username: string;
+  score: number;
+  avatar_url: string | null;
+  games_played: number;
+  wins: number;
+  win_rate: number;
+}
