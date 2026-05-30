2f3285615ae5a209c8e132b802eee737731f6dcf
core2drew <calupesandro@gmail.com>
2026-05-30 16:20:10 +0800

    chore: hide correct answer and clear session upon exiting quiz

--- src/app/pages/quiz-page/components/screens/wrong-answer-screen/wrong-answer-screen.ts
+++ src/app/pages/quiz-page/components/screens/wrong-answer-screen/wrong-answer-screen.ts
@@ -1,17 +1,19 @@
-import { Component, inject, Input, input, signal } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { LucideX, LucideCheck } from '@lucide/angular';
+import { LucideX } from '@lucide/angular';
+import { SessionService } from '@services/session/session.service';
 import { getWrongRandomInspirationalMessage } from '@utils/inspirational-message-utils';
 
 @Component({
   selector: 'app-wrong-answer-screen',
-  imports: [LucideX, LucideCheck],
+  imports: [LucideX],
   templateUrl: './wrong-answer-screen.html',
   styleUrl: './wrong-answer-screen.css',
   standalone: true,
 })
 export class WrongAnswerScreen {
   private router = inject(Router);
+  private sessionService = inject(SessionService);
 
   showContent = signal(true);
   shakeActive = signal(true);
@@ -21,6 +23,7 @@ export class WrongAnswerScreen {
   @Input() correctAnswer: string | undefined = '';
 
   exitQuiz(): void {
+    this.sessionService.clearSession();
     this.router.navigate(['/']);
   }
 }
