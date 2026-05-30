6ffeb61d372e56c3e382b449003cfc80a604e254
core2drew <calupesandro@gmail.com>
2026-05-31 00:26:47 +0800

    chore: add quiz session status

---
+++ functions/src/models/room.model.ts
@@ -0,0 +1,5 @@
+export enum QuizSessionStatus {
+  WAITING = 1,
+  STARTED = 2,
+  ENDED = 3,
+}
--- src/app/models/quiz/quiz-session.model.ts
+++ src/app/models/quiz/quiz-session.model.ts
@@ -1,6 +1,18 @@
 import { Timestamp } from 'firebase/firestore';
 
+export enum QuizSessionStatus {
+  WAITING = 1,
+  STARTED = 2,
+  ENDED = 3,
+}
+export const QuizSessionStatusLabel: Record<QuizSessionStatus, string> = {
+  [QuizSessionStatus.WAITING]: 'Waiting',
+  [QuizSessionStatus.STARTED]: 'Started',
+  [QuizSessionStatus.ENDED]: 'Ended',
+};
+
 export interface QuizSession {
   currentQuestionIndex: number;
   questionTimerExpiresAt: Timestamp;
+  status: QuizSessionStatus;
 }
