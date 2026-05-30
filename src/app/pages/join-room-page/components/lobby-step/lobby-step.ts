1914ed15d89c587ed86eb8a16ae05759fa9fdfd8
core2drew <calupesandro@gmail.com>
2026-05-31 00:29:33 +0800

    refactor: integrate quiz session status

--- src/app/pages/join-room-page/components/lobby-step/lobby-step.ts
+++ src/app/pages/join-room-page/components/lobby-step/lobby-step.ts
@@ -1,19 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, signal, Signal } from '@angular/core';
+import { Component, inject, signal, Signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { Player, PlayerStatus, PlayerWithUIData } from '@models/quiz/player.model';
+import { Player } from '@models/quiz/player.model';
 
-import { RoomService } from '@services/room/room.service';
-import { sessionStore } from '@stores/session.store';
 import { TableModule } from 'primeng/table';
-import { Subscription } from 'rxjs';
+import { filter, switchMap } from 'rxjs';
 import { LogoTitle } from '../logo-title/logo-title';
 import { IdleTextDot } from '../../../../components/idle-text-dot/idle-text-dot';
 import { GameService } from '@services/quiz/game.service';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { getAvatarColorByName } from '@utils/player-utils';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { Avatar } from '../../../../components/avatar/avatar';
 import { SessionService } from '@services/session/session.service';
+import { QuizSessionStatus } from '@models/quiz/quiz-session.model';
 
 @Component({
   selector: 'app-lobby-step',
@@ -23,55 +21,47 @@ import { SessionService } from '@services/session/session.service';
   standalone: true,
 })
 export class LobbyStep {
-  private router = inject(Router);
-  private roomService = inject(RoomService);
-  private gameService = inject(GameService);
-  private sessionService = inject(SessionService);
-
-  private gameStartSub!: Subscription;
-  private gameEndSub!: Subscription;
+  private readonly sessionService = inject(SessionService);
+  private readonly router = inject(Router);
+  private readonly gameService = inject(GameService);
 
   players: Signal<Player[] | undefined> = signal(undefined);
 
   constructor() {
-    const roomId = sessionStore.getValue().roomId;
-    if (!roomId) {
-      console.error('No active room found, redirecting back to home.');
-      this.router.navigate(['/']);
-      return;
-    }
-
-    this.players = toSignal(this.gameService.getPlayersInRoom(roomId!));
-
-    this.gameStartSub = this.roomService.waitUntilGameStarts(roomId).subscribe({
-      next: (isStarted) => {
-        if (isStarted) {
-          console.log('Game has started! Redirecting to arena...');
-          this.router.navigate(['/quiz-blitz']);
-        }
-      },
-      error: (err) => console.error('Error listening to room status:', err),
-    });
-
-    this.gameEndSub = this.roomService.waitUntilGameEnds(roomId).subscribe({
-      next: (isEnded) => {
-        if (isEnded) {
-          this.sessionService.leaveRoom();
-          console.log('Game has ended! Redirecting to home...');
-          location.reload();
-        }
-      },
-      error: (err) => console.error('Error listening to room status:', err),
-    });
+    this.players = toSignal(this.gameService.getPlayersInRoom(this.sessionService.roomId()!));
+    this.listenToGameEvents();
   }
 
-  ngOnDestroy(): void {
-    if (this.gameStartSub) {
-      this.gameStartSub.unsubscribe();
-    }
+  private listenToGameEvents(): void {
+    // Convert the roomId Signal to an Observable that reacts if the ID changes
+    const roomId$ = toObservable(this.sessionService.roomId).pipe(
+      filter((id): id is string => !!id), // Only proceed if id is truthy
+    );
 
-    if (this.gameEndSub) {
-      this.gameEndSub.unsubscribe();
-    }
+    // 1. Handle Game Start
+    roomId$
+      .pipe(
+        switchMap((id) => this.gameService.watchQuizSessionStatus(id)),
+        takeUntilDestroyed(), // Automatically cleans up when component destroys
+      )
+      .subscribe({
+        next: (status) => {
+          if (status === QuizSessionStatus.WAITING) {
+            this.router.navigate(['/join']);
+            return;
+          }
+          if (status === QuizSessionStatus.STARTED) {
+            this.router.navigate(['/quiz-blitz']);
+
+            return;
+          }
+          if (status === QuizSessionStatus.ENDED) {
+            this.router.navigate(['/']);
+            this.sessionService.clearSession();
+            return;
+          }
+        },
+        error: (err) => console.error('Error listening to game start:', err),
+      });
   }
 }
