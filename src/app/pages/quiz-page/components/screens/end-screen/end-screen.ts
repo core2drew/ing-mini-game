c26c49f503f78d2f3c2b68f17a80427f98caa922
core2drew <calupesandro@gmail.com>
2026-06-01 08:53:34 +0800

    chore: update end screen

--- src/app/pages/quiz-page/components/screens/end-screen/end-screen.ts
+++ src/app/pages/quiz-page/components/screens/end-screen/end-screen.ts
@@ -2,21 +2,27 @@ import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from '@services/session/session.service';
 import { getCorrectRandomInspirationalMessage } from '@utils/inspirational-message-utils';
+import { Avatar } from '../../../../../components/avatar/avatar';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { PlayerService } from '@services/quiz/player.service';
 
 @Component({
   selector: 'app-end-screen',
-  imports: [],
+  imports: [Avatar],
   templateUrl: './end-screen.html',
   styleUrl: './end-screen.css',
 })
 export class EndScreen {
-  private sessionService = inject(SessionService);
+  private playerService = inject(PlayerService);
+  private readonly sessionService = inject(SessionService);
   private router = inject(Router);
 
   confetti = signal<{ x: number; delay: number; color: string; size: number; dur: number }[]>([]);
   showContent = signal(false);
   inspirationalMessage = signal(getCorrectRandomInspirationalMessage());
-
+  private readonly roomId = this.sessionService.roomId();
+  private readonly playerName = this.sessionService.playerName();
+  readonly player = toSignal(this.playerService.getPlayer(this.roomId!, this.playerName!));
   private colors = ['#22c55e', '#86efac', '#4ade80', '#fbbf24', '#34d399', '#a3e635'];
 
   ngOnInit(): void {
@@ -30,9 +36,5 @@ export class EndScreen {
       })),
     );
     setTimeout(() => this.showContent.set(true), 100);
-  }
-
-  close() {
-    this.router.navigate(['/join']);
   }
 }
