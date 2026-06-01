5ae8cd5d13df4a05f539ab4b79d921aa786e88c7
core2drew <calupesandro@gmail.com>
2026-06-01 12:47:07 +0800

    refactor(room-guard): enhance player signal handling and room ID checks

--- src/app/guards/room-guard/room-guard.ts
+++ src/app/guards/room-guard/room-guard.ts
@@ -1,17 +1,40 @@
-import { inject } from '@angular/core';
+import { effect, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CanActivateFn, Router } from '@angular/router';
-import { RoomService } from '@services/room/room.service';
+import { PlayerStatus } from '@models/quiz/player.model';
+
+import { PlayerService } from '@services/quiz/player.service';
 import { sessionStore } from '@stores/session.store';
 
 export const roomGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
-  const roomService = inject(RoomService);
+
+  const playerService = inject(PlayerService);
+  const roomId = sessionStore.getValue().roomId;
+  const playerName = sessionStore.getValue().name;
+  // We need to convert the observable to a signal so that we can use it in the guard
+  const player = toSignal(playerService.getPlayer(roomId!, playerName!)); // Wait for the player signal to emit its first value
+
+  await new Promise((resolve) => {
+    const effectRef = effect(() => {
+      if (player() !== undefined) {
+        effectRef.destroy();
+        resolve(true);
+      }
+    });
+  });
 
   const hasRoomId = !!localStorage.getItem('roomId') || !!sessionStore.getValue().roomId;
 
-  if (!hasRoomId) {
+  if (!hasRoomId || !player()) {
     router.navigate(['/join']);
     return false;
   }
+
+  if (player()?.status === PlayerStatus.WAITING) {
+    router.navigate(['/join']);
+    return false;
+  }
+
   return true;
 };
